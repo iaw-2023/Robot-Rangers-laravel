18b11810aebe124c07b41cb08da4e1138b5c2757
Gonzalo <89589881+GonzaP14@users.noreply.github.com>
2023-04-26 15:59:05 -0300

    Controllers

--- app/Http/Controllers/MarcasController.php
+++ app/Http/Controllers/MarcasController.php
@@ -4,17 +4,62 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Marca;
 
 class MarcasController extends Controller
 {
-    public function index(){ 
-        $marcas = Marca::all();
-        
-        if ($marcas->count() > 0) {
-            return response()->json(['status'=>200,'marcas'=>$marcas], 200);
-        } else {
-            return response()->json(['status'=>404,'no marcas found'], 404);
-        }
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
     }
 }
