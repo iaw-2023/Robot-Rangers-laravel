ad42ca82ed756fa4a23447c992cc9730f2722571
nico23112001 <nmessina@live.com.ar>
2023-04-27 19:01:03 -0300

    Laravel ui/bootstrap y cambios en controllers

--- app/Http/Controllers/Web/DetallePedidoController.php
+++ app/Http/Controllers/Web/DetallePedidoController.php
@@ -18,53 +18,10 @@ class DetallePedidosController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('detallePedidos.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreDetallePedidoRequest $request)
-    {
-        DetallePedido::create([$request->validated()]);
-        return back()->with('message', 'Detalle Pedido has been created.');
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(string $id)
     {
         return view('detallePedidos.show', ['detallePedidos', DetallePedido::findOrFail($id)]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        return view('detallePedidos.edit',['detallePedidos' => DetallePedido::where('id', $id)->first()]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateDetallePedidoRequest $request, string $id)
-    {
-        DetallePedido::where('id', $id)->update($request->validated());
-        return back()->with('message', 'Detalle Pedido has been updated.');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        DetallePedido::destroy($id);
-        return back()->with('message', 'Detalle Pedido has been deleted.');
     }
 }
--- app/Http/Controllers/Web/PedidoController.php
+++ app/Http/Controllers/Web/PedidoController.php
@@ -19,53 +19,10 @@ class PedidosController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        return view('pedidos.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StorePedidoRequest $request)
-    {
-        Pedido::create([$request->validated()]);
-        return back()->with('message', 'Pedido has been created.');
-    }
-
-    /**
      * Display the specified resource.
      */
     public function show(string $id)
     {
         return view('pedidos.show', ['pedidos', Pedido::findOrFail($id)]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        return view('pedidos.edit',['pedidos' => Pedido::where('id', $id)->first()]);
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdatePedidoRequest $request, string $id)
-    {
-        Pedido::where('id', $id)->update($request->validated());
-        return back()->with('message', 'Pedido has been updated.');
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        Pedido::destroy($id);
-        return back()->with('message', 'Pedido has been deleted.');
     }
 }
