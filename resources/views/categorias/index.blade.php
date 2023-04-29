944828697c5e10f2c6773a22077c680d5928f7aa
nico23112001 <62406648+nico23112001@users.noreply.github.com>
2023-04-29 18:32:28 -0300

    Correcion de color en index y agregue para ir a create con un boton en categorias

--- resources/views/categorias/index.blade.php
+++ resources/views/categorias/index.blade.php
@@ -3,7 +3,8 @@
 @section('content')
 <div class="container">
     @include('messages')
-    <table class="table">
+    <a href="/categorias/create" class="btn btn-success">Crear categoria</a>
+    <table class="table border-b border-gray-100 dark:border-gray-700">
         <thead>
             <tr>
             <th scope="col" style="color:white">ID</th>
