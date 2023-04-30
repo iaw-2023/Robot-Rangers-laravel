230623cb26f36604194ef4737ebb250c35321673
Gonzalo <89589881+GonzaP14@users.noreply.github.com>
2023-04-30 19:07:50 -0300

    Mejora en la vista de navegadores white

--- resources/views/categorias/create.blade.php
+++ resources/views/categorias/create.blade.php
@@ -2,22 +2,22 @@
 
 @section('content')
 <div class="container" >
-<div class="card col-6 offset-3 bg-gray-100 dark:bg-gray-900 shadow"style="border-color:black" >
-  <h5 class="card-header bg-gray-100 dark:bg-gray-900" style="color:white">Crear categoria</h5>
+<div class="card col-6 offset-3 bg-gray-800 dark:bg-gray-900 shadow"style="border-color:black" >
+  <h5 class="card-header bg-gray-800 dark:bg-gray-900" style="color:white">Crear categoria</h5>
   <div class="card-body">
   @include('messages')
      <form action="/categorias" method="POST">
         @csrf
-        <div class="mb-3 bg-gray-100 dark:bg-gray-900" style="color:white">
-            <label class="form-label bg-gray-100 dark:bg-gray-900" style="color:white">Nombre</label>
-            <input type="text" name="nombre"   class="form-control @error('nombre') is-invalid @enderror" value="{{old('nombre')}}">
+        <div class="mb-3 bg-gray-800 dark:bg-gray-900" style="color:white">
+            <label class="form-label bg-gray-800 dark:bg-gray-900" style="color:white">Nombre</label>
+            <input type="text" name="nombre" class="form-control @error('nombre') is-invalid @enderror" value="{{old('nombre')}}">
             @error('nombre')
                 <span class="text-danger">
                     <strong>{{$message}}</strong>
                 </span>
             @enderror
         </div>
-        <div class="mb-3 bg-gray-100 dark:bg-gray-900" style="color:white">
+        <div class="mb-3 bg-gray-800 dark:bg-gray-900" style="color:white">
             <button type="submit" class="btn btn-success">Enviar</button>
         </div>
      </form>
