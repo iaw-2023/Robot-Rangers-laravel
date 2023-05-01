ab18294a2d0d56e389807be72e987206d8b187f7
nico23112001 <62406648+nico23112001@users.noreply.github.com>
2023-05-01 18:37:02 -0300

    Dashboard mejorado

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,63 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-md-3 mb-3">
-        <div class="card bg-primary text-white h-100">
-            <div class="card-body py-5">Primary Card</div>
-            <div class="card-footer d-flex">
-            View Details
-            <span class="ms-auto">
-                <i class="bi bi-chevron-right"></i>
-            </span>
+<div class="container">
+  <div class="row">
+    <div class="col-md-2"></div>
+    <div class="col-md-8 d-flex flex-wrap justify-content-center">
+        <div class="card bg-success text-white m-3" style="width: 18rem;">
+            <div class="card-header">Categorias</div>
+                <div class="card-body py-5 d-grid">
+                    <a class="btn btn-dark btn-block mb-3" href="/categorias/create" role="button">Crear categoria</a>
+                    <a class="btn btn-dark btn-block" href="/categorias" role="button">Ver categorias</a>
+                </div>
+        </div>
+        <div class="card bg-success text-white m-3" style="width: 18rem;">
+        <div class="card-header">Marcas</div>
+            <div class="card-body py-5 d-grid">
+                <a class="btn btn-dark btn-block mb-3" href="/marcas/create" role="button">Crear marca</a>
+                <a class="btn btn-dark btn-block" href="/marcas" role="button">Ver marcas</a>
             </div>
         </div>
-        </div>
-        <div class="col-md-3 mb-3">
-        <div class="card bg-warning text-dark h-100">
-            <div class="card-body py-5">Warning Card</div>
-            <div class="card-footer d-flex">
-            View Details
-            <span class="ms-auto">
-                <i class="bi bi-chevron-right"></i>
-            </span>
+        <div class="card bg-success text-white m-3" style="width: 18rem;">
+        <div class="card-header">Prendas</div>
+            <div class="card-body py-5 d-grid">
+                <a class="btn btn-dark btn-block mb-3" href="/prendas/create" role="button">Crear prenda</a>
+                <a class="btn btn-dark btn-block" href="/prendas" role="button">Ver prendas</a>
             </div>
         </div>
-        </div>
-        <div class="col-md-3 mb-3">
-        <div class="card bg-success text-white h-100">
-            <div class="card-body py-5">Success Card</div>
-            <div class="card-footer d-flex">
-            View Details
-            <span class="ms-auto">
-                <i class="bi bi-chevron-right"></i>
-            </span>
+        <div class="card bg-success text-white m-3" style="width: 18rem;">
+        <div class="card-header">Pedidos:</div>
+            <div class="card-body py-5 d-grid">
+                <a class="btn btn-dark btn-block" href="/pedidos" role="button">Ver pedidos</a>
             </div>
         </div>
-        </div>
-        <div class="col-md-3 mb-3">
-        <div class="card bg-danger text-white h-100">
-            <div class="card-body py-5">Danger Card</div>
-            <div class="card-footer d-flex">
-            View Details
-            <span class="ms-auto">
-                <i class="bi bi-chevron-right"></i>
-            </span>
+        <div class="card bg-success text-white m-3" style="width: 18rem;">
+        <div class="card-header">Detalle Pedidos:</div>
+            <div class="card-body py-5 d-grid">
+                <a class="btn btn-dark btn-block" href="/detallePedidos" role="button">Ver detalles de pedidos</a>
             </div>
-        </div>
-        </div>
-        <div class="col-md-3 mb-3">
-        <div class="card bg-primary text-white h-100">
-            <div class="card-body py-5">Primary Card</div>
-            <div class="card-footer d-flex">
-            View Details
-            <span class="ms-auto">
-                <i class="bi bi-chevron-right"></i>
-            </span>
-            </div>
-        </div>
         </div>
     </div>
+    <div class="col-md-2"></div>
+  </div>
 </div>
+
 @endsection
