60cd89d9ed5b2be92f805f47454dc1a99d8bf38c
nico23112001 <62406648+nico23112001@users.noreply.github.com>
2023-05-19 18:49:32 -0300

    Update PrendaController.php

--- app/Http/Controllers/Api/PrendaController.php
+++ app/Http/Controllers/Api/PrendaController.php
@@ -151,7 +151,7 @@ class PrendaController extends ApiController
             ->when($request->has('color'), fn($query) => $query->whereRaw('LOWER(color) = ?', [strtolower($request->input('color'))]))
             ->when($request->has('precio'), fn($query) => $query->orderBy('precio', $request->input('precio')));
 
-        $result = $prendas->get()->paginate(10);
+        $result = $prendas->paginate(10);
 
         if ($result->isEmpty()) {
             return response()->json(['message' => 'Prendas not found'], 404);
